c8d0e2f54f6aa17edf6a49aa4cb581e437640bfe
Diego Ribeiro Chaves <drchaves@inf.ufsm.br>
2024-09-28 17:57:04 -0300

    feat: metodo da secante atualizado

--- python-projects/metodo-secante.py
+++ python-projects/metodo-secante.py
@@ -7,18 +7,16 @@ def f(x):
     return x ** 3 - 2 * x ** 2 - 4 * x + 4
 
 def printa_iteracao(n, xn, fxn, erro_relativo):
+    # Formatação da saída
     if n == 0:
-        #print(f"n = {n} | xn = {xn} | f(xn) = {fxn} | ERn = -")
-        output = f"n = {n} | xn = {xn:.6f} | f(xn) = {fxn:.6f} | ERn = -"
+        output = f"{n:<10} | {xn:.6f} | {fxn:.6f} | -"
     else:
-        #print(f"n = {n} | xn = {xn} | f(xn) = {fxn} | ERn = {erro_relativo}")
-        output = f"n = {n} | xn = {xn:.6f} | f(xn) = {fxn:.6f} | ERn = {erro_relativo:.6f}"
+        output = f"{n:<10} | {xn:.6f} | {fxn:.6f} | {erro_relativo:.6f}"
 
     print(output)
 
+    # Escreve no PDF
     pdf.cell(0, 10, output, ln=True)
-
-# precisamos de dois chutes iniciais
 
 # Criando o PDF
 pdf = FPDF()
@@ -26,32 +24,36 @@ pdf.add_page()
 pdf.set_font("Arial", size=12)
 pdf.cell(0, 10, "Metodo da Secante, resolucao da funcao x^3 - 2x^2 - 4x + 4, intervalo [-2, -1]", ln=True)
 
+# Adiciona cabeçalho da tabela ao PDF
+pdf.cell(0, 10, "Iteração | x_n      | f(x_n)   | ER_n     ", ln=True)
+pdf.cell(0, 10, "-" * 50, ln=True)
+
+# Imprimir cabeçalho da tabela no console
+print(f"{'Iteração':<10} | {'x_n':<10} | {'f(x_n)':<10} | {'ER_n':<10}")
+print("-" * 50)
+
 chute_inicial_1 = -2.0
 chute_inicial_2 = -1.0
 
-valores_x = []
-
-valores_x.append(chute_inicial_1)
-valores_x.append(chute_inicial_2)
+valores_x = [chute_inicial_1, chute_inicial_2]
 
 erro_relativo = 1.0
 
 while erro_relativo > ERRO:
-
     xn = valores_x[-1]
     fxn = f(xn)
     xn1 = valores_x[-2]
     fxn1 = f(xn1)
 
+    # Chamada da função para printar a iteração
     printa_iteracao(n, xn, fxn, erro_relativo)
     n += 1
 
     chute = xn - (fxn * ((xn - xn1) / (fxn - fxn1)))
     valores_x.append(chute)
 
+    # Cálculo do erro relativo
     erro_relativo = abs(valores_x[-1] - valores_x[-2])
-
-#print(f"raiz aproximada = {valores_x[-1]}")
 
 # Finaliza o PDF
 pdf.cell(0, 10, f"Raiz aproximada = {valores_x[-1]:.6f}", ln=True)
